5a96fbc18b546bcd022eb2a43401858ed84a7724
arthu <jordnhouyon@outlook.fr>
2023-01-09 01:53:06 +0100

    jeu de la taupe ok

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole');
+const scoreBoard =$('.score');
+const moles =$('.mole');
+
+function startGame() {
+    const gameTime = 10000;
+
+    let score = 0
+
+
+    let startTime = setInterval(function () {
+        let hole = holes.eq(Math.floor(Math.random() * holes.length));
+        hole.addClass('up');
+
+        let duration = Math.random() * (1000 - 200) + 200;
+        setTimeout(function () {
+            hole.removeClass('up');
+        }, duration);
+
+        hole.click(function () {
+            score++;
+            $(this).removeClass('up');
+            scoreBoard.text(score)
+        });
+    }, 200);
+
+    setTimeout(function () {
+        clearInterval(startTime);
+        alert("La partie est terminé! ")
+
+    }, gameTime);
+
+}
